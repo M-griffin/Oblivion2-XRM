e21d8aaef8f32d8720a824c14c31728636d7ae7b
M-Griffin <mrmisticismo@hotmail.com>
2017-03-18 03:13:40 -0500

    Started on Code Mapping for ACS.

--- src/access_condition.cpp
+++ src/access_condition.cpp
@@ -1,18 +1,13 @@
 #include "access_condition.hpp"
+#include "model/users.hpp"
 
+#include <boost/algorithm/string.hpp>
+#include <boost/make_shared.hpp>
 
 #include <string>
+#include <vector>
 #include <iostream>
 #include <sstream>
-
-
-AccessCondition::AccessCondition()
-{
-}
-
-AccessCondition::~AccessCondition()
-{
-}
 
 
 // Pass data strings to m_session_io.parseToCodeMap()
@@ -20,3 +15,95 @@ AccessCondition::~AccessCondition()
 
 // Build methods to to parse secrity, flags, and others from obv2.
 // methods should return boolean tests to validate user access.
+
+
+/**
+ * @brief Parse Code Map and Test Secutiry and AR Flags.
+ * @param code_map
+ * @param user
+ * @return 
+ */
+bool AccessCondition::parseCodeMap(const std::vector<MapType> &code_map, user_ptr user)
+{
+    bool condition = false;
+      
+    MapType my_matches;
+
+    // Make a copy so the original is not modified.
+    std::vector<MapType> code_mapping;
+    code_mapping.assign(code_map.begin(), code_map.end());
+
+    // All Global MCI Codes likes standard screens and colors will
+    // He handled here, then specific interfaces will break out below this.
+    // Break out parsing on which pattern was matched.
+    while(code_mapping.size() > 0)
+    {
+        // Loop Backwards to perserve string offsets on replacement.
+        my_matches = code_mapping.back();
+        code_mapping.pop_back();
+
+        // Check for Custom Screen Translation Mappings
+        // If these exist, they take presidence over standard codes
+        //if (m_mapped_codes.size() > 0)
+        //{
+            
+            /*
+            std::map<std::string, std::string>::iterator it;
+            it = m_mapped_codes.find(my_matches.m_code);
+            if (it != m_mapped_codes.end())
+            {
+                //std::cout << "gen found: " << my_matches.m_code << " : " << it->second << std::endl;
+                // If found, replace mci sequence with text
+                ansi_string.replace(my_matches.m_offset, my_matches.m_length, it->second);
+            }
+            else
+            {
+                //std::cout << "gen not found: " << my_matches.m_code << std::endl;
+                std::string remove_code = "";
+                ansi_string.replace(my_matches.m_offset, my_matches.m_length, remove_code);
+            }*/
+        //}
+    }  
+    
+    return condition;
+}
+
+
+/**
+ * @brief Parse ASC Strings then test User Flags
+ * @param acs_string
+ * @param user
+ * @return 
+ */
+bool AccessCondition::parseAcsString(std::string &acs_string, user_ptr user)
+{
+    bool condition = false;
+    std::vector<MapType> code_map;
+    
+    // First split string 
+    if (user)
+    {
+        // First split any OR statements.
+        std::vector<std::string> tokens;
+        boost::split(tokens, acs_string, boost::is_any_of("|"));
+
+        if (tokens.size() > 1)
+        {
+            // Then we have multiple statements.
+            for (std::string t : tokens)
+            {
+                std::vector<MapType> tmp = m_session_io.parseToCodeMap(t, ACS_EXPRESSION);          
+                code_map.insert( code_map.end(), tmp.begin(), tmp.end() );
+            }
+        }
+        else 
+        {
+            code_map = m_session_io.parseToCodeMap(acs_string, ACS_EXPRESSION);
+        }
+        
+        // Parse code map to test Secutiry and AR Flags.
+        condition = parseCodeMap(code_map, user);        
+    }
+    
+    return condition;
+}
--- src/access_condition.hpp
+++ src/access_condition.hpp
@@ -2,9 +2,16 @@
 #define ACCESS_CONDITION_HPP
 
 #include "session_io.hpp"
+#include "model/structures.hpp"
+
+#include <boost/smart_ptr/shared_ptr.hpp>
 
 #include <string>
+#include <vector>
 #include <map>
+
+class Users;
+typedef boost::shared_ptr<Users> user_ptr;
 
 /**
  * @class AccessCondition
@@ -16,9 +23,25 @@
 class AccessCondition
 {
 public:
-    AccessCondition();
-    ~AccessCondition();
+    AccessCondition()  { };
+    ~AccessCondition() { };
     
+    /**
+     * @brief Parse Code Map and Test Secutiry and AR Flags.
+     * @param code_map
+     * @param user
+     * @return 
+     */
+    bool parseCodeMap(const std::vector<MapType> &code_map, user_ptr user);
+
+    /**
+     * @brief Parse ASC Strings then test User Flags
+     * @param acs_string
+     * @param user
+     * @return 
+     */
+    bool parseAcsString(std::string &acs_string, user_ptr user);
+
     // Using Session IO for Code Mapping
     SessionIO  m_session_io;
 
@@ -26,7 +49,7 @@ public:
     // start with NOT s255 then test for s255
     // start with all not, but all normal will get caught and never pass through.
     // seperate expressons with | or. and ( )
-    const std::string STD_EXPRESSION = {"([|]{1}[0-9]{2})|([|]{1}[X][Y][0-9]{4})|"
+    const std::string ACS_EXPRESSION = {"([|]{1}[0-9]{2})|([|]{1}[X][Y][0-9]{4})|"
                                         "([|]{1}[A-Z]{1,2}[0-9]{1,2})|([|]{1}[A-Z]{2})|"
                                         "([%]{2}[\\w]+[.]{1}[\\w]{3})|([%]{1}[A-Z]{2})|"
                                         "([%]{1}[0-9]{2})"
