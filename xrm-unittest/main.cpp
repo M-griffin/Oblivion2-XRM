9d818a23819e361ae77c1ef15bdeb4c98d84459c
M-Griffin <mrmisticismo@hotmail.com>
2017-04-09 01:51:17 -0500

    Completed initial Unit Tests of Form Data Mapping to Config Class

--- xrm-unittest/main.cpp
+++ xrm-unittest/main.cpp
@@ -34,14 +34,30 @@ std::string GLOBAL_TEXTFILE_PATH = "";
 std::string USERS_DATABASE       = "";
 
 
+
+// Definitions for XRMFormSystemConfig Unit Tests.
+#define MAX_OPTIONS 106
+#define BBS_NAME_SYSOP 0
+#define USE_SSL_SERVICE 9
+#define PORT_TELNET 6
+
+
 /**
  * @brief Unit Testing for Initial Form Configuration Implimentation
  * @return 
  */
 SUITE(XRMFormSystemConfig)
 {
-    // Just testing output of onEnter .. 
-    TEST(SystemConfigOutputTest)
+    /**
+     * Just testing output of onEnter .. And generate mapping translation mock up.
+     * In generate config value are places in menu options form_value as strings
+     * They are then edited and updated.  YAML::Node then translates and maps
+     * Back to their original types changing Strings back to String, Bool, Int etc..
+     * These tests confirm the mapping and updates are working correctly for the form interface.
+     */
+    
+    // Tests Node Type Casting from String to String
+    TEST(SystemConfigOutputTest_ThenStringReassignment)
     {        
         config_ptr config(new Config());
         form_ptr form(new FormSystemConfig(config));
@@ -52,9 +68,107 @@ SUITE(XRMFormSystemConfig)
         std::cout << "opts size: " << opts.size() << std::endl;
         
         // 106 Config Options generated.
-        CHECK(opts.size() == 106);        
+        CHECK(opts.size() == MAX_OPTIONS);
+        
+        // Setup Yaml Mapping, and assign the config to it.
+        YAML::Node node;
+        node = config;
+        
+        // Test we can Read Each Value and the mappings match
+        std::cout << "Name[0]: " << opts[BBS_NAME_SYSOP].name << std::endl;        
+        std::cout << "Node Value: " << node[opts[BBS_NAME_SYSOP].name] << std::endl;
+        
+        // Test Value.
+        CHECK(opts[BBS_NAME_SYSOP].form_value == node[opts[BBS_NAME_SYSOP].name].as<std::string>());
+        
+        // Update Value on Option.. 
+        opts[BBS_NAME_SYSOP].form_value = "new value";
+        
+        // Assign new value by lookup
+        node[opts[BBS_NAME_SYSOP].name] = opts[BBS_NAME_SYSOP].form_value;
+        
+        // Move from Node back to Config and Translations String to String
+        Config c = node.as<Config>();
+        
+        // Now test new value is populated in config class.
+        CHECK(opts[BBS_NAME_SYSOP].form_value == c.bbs_name_sysop);
     }
     
+    // Tests Node Type Casting from String to Boolean
+    TEST(SystemConfigOutputTest_ThenBoolReassignment)
+    {        
+        config_ptr config(new Config());
+        form_ptr form(new FormSystemConfig(config));
+        
+        // run the form on enter.
+        form->onEnter();
+        std::vector<MenuOption> opts = form->baseGetFormOptions();
+        std::cout << "opts size: " << opts.size() << std::endl;
+        
+        // 106 Config Options generated.
+        CHECK(opts.size() == MAX_OPTIONS);
+        
+        // Setup Yaml Mapping, and assign the config to it.
+        YAML::Node node;
+        node = config;
+        
+        // Test we can Read Each Value and the mappings match
+        std::cout << "Name[0]: " << opts[USE_SSL_SERVICE].name << std::endl;        
+        std::cout << "Node Value: " << node[opts[USE_SSL_SERVICE].name] << std::endl;
+        
+        // Test Value.
+        CHECK(!node[opts[USE_SSL_SERVICE].name].as<bool>());
+        
+        // Update Value on Option.. 
+        opts[USE_SSL_SERVICE].form_value = "true";
+        
+        // Assign new value by lookup
+        node[opts[USE_SSL_SERVICE].name] = opts[USE_SSL_SERVICE].form_value;
+        
+        // Move from Node back to Config and Translations String to Bool
+        Config c = node.as<Config>();
+        
+        // Now test new value is populated in config class.
+        CHECK(c.use_service_ssl == true);
+    }
+    
+    // Tests Node Type Casting from String to Integer
+    TEST(SystemConfigOutputTest_ThenIntegerReassignment)
+    {        
+        config_ptr config(new Config());
+        form_ptr form(new FormSystemConfig(config));
+        
+        // run the form on enter.
+        form->onEnter();
+        std::vector<MenuOption> opts = form->baseGetFormOptions();
+        std::cout << "opts size: " << opts.size() << std::endl;
+        
+        // 106 Config Options generated.
+        CHECK(opts.size() == MAX_OPTIONS);
+        
+        // Setup Yaml Mapping, and assign the config to it.
+        YAML::Node node;
+        node = config;
+        
+        // Test we can Read Each Value and the mappings match
+        std::cout << "Name[0]: " << opts[PORT_TELNET].name << std::endl;        
+        std::cout << "Node Value: " << node[opts[PORT_TELNET].name] << std::endl;
+        
+        // Test Value.
+        CHECK(node[opts[PORT_TELNET].name].as<int>() == 6023);
+        
+        // Update Value on Option.. 
+        opts[PORT_TELNET].form_value = "2323";
+        
+        // Assign new value by lookup
+        node[opts[PORT_TELNET].name] = opts[PORT_TELNET].form_value;
+        
+        // Move from Node back to Config and Translations String to Int
+        Config c = node.as<Config>();
+        
+        // Now test new value is populated in config class.
+        CHECK(c.port_telnet == 2323);
+    }
 
 }
 
