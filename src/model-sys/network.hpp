e46abd86af5e9bc6400c59cc295ad8235d4a5354
M-Griffin <mrmisticismo@hotmail.com>
2017-05-03 18:49:54 -0500

    Change Log Update (00.00.90) * 2017-05-03 + Added new model files for File Areas, Message Areas, Network Addresses   and BBS Listing. - Create Dao objects for reading/writing the database for the new models.

---
+++ src/model-sys/network.hpp
@@ -0,0 +1,38 @@
+#ifndef NETWORK_HPP
+#define NETWORK_HPP
+
+#include <boost/smart_ptr/shared_ptr.hpp>
+
+class Network;
+typedef boost::shared_ptr<Network> network_ptr;
+
+/**
+ * @class Network
+ * @author Michael Griffin
+ * @date 03/05/2017
+ * @file network.hpp
+ * @brief Network Formats: Type - Fido, IP Address
+ */
+class Network
+{
+public:
+  
+    long iId;
+    std::string sName
+    std::string sType;
+    std::string sAddress
+    
+    Network()
+        : iId(-1)
+        , sName("")
+        , sType("")
+        , sAddress("")
+    { }
+    
+    ~Network()
+    { }
+
+};
+
+#endif // NETWORK_HPP
+
