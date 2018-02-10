14afabf66f79df5d38f629f53ac2ec2e8155e163
Admin <admin@Admins-Mac.local>
2018-02-10 03:40:01 +0300

    Add to files that were renamed.

---
+++ sqlitewrap/SysLogs.cpp
@@ -0,0 +1,54 @@
+/**
+ *	SysLog.cpp
+ *
+ * Rewritten / author: 2016-02-19 / mrmisticismo@hotmail.com
+ * Published / author: 2005-08-12 / grymse@alhem.net
+ * Copyright (C) 2015-2016  Michael Griffin
+ * Copyright (C) 2001-2006  Anders Hedstrom
+ * This program is made available under the terms of the GNU GPL.
+ */
+#ifndef _WIN32
+
+#include <sqlite3.h>
+#include <syslog.h>
+
+#include "Database.h"
+#include "Query.h"
+#include "IError.h"
+#include "SysLogs.h"
+
+namespace SQLW
+{
+SysLog::SysLog(const std::string& appname,int option,int facility)
+{
+    openlog(appname.c_str(), option, facility);
+}
+
+SysLog::~SysLog()
+{
+    closelog();
+}
+
+void SysLog::databaseError(Database& db,const std::string& str)
+{
+    if (!db.isConnected())
+    {
+        std::cout << "Database Not Connected!" << std::endl;
+    }
+    syslog(LOG_ERR, "%s", str.c_str());
+}
+
+void SysLog::databaseError(Database& db,Query& q,const std::string& str)
+{
+    if (!db.isConnected())
+    {
+        std::cout << "Database Not Connected!" << std::endl;
+    }
+    syslog(LOG_ERR, "%s: %s(%d)", str.c_str(),q.GetError().c_str(),q.GetErrno());
+    //syslog(LOG_ERR, "QUERY: \"%s\"", q.GetLastQuery().c_str());
+}
+
+} // namespace SQLW
+
+
+#endif // WIN32
